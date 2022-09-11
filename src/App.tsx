a8e5fc084e032157b070915b424dafdf02bceef6
kwantke <kokk7310@gamil.com>
2022-09-11 22:17:38 +0900

    Making WordList

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Day from './component/Day';
+import DayList from './component/DayList';
+import EmptyPage from './component/EmptyPage';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+       <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path="/">
+            <DayList/>
+          </Route>
+          <Route path="/day/:day">
+            <Day/>
+          </Route>
+          <Route>
+            <EmptyPage/>
+          </Route>
+        </Switch>
+        
+      </div> 
+    </BrowserRouter>
   );
+
 }
 
 export default App;
